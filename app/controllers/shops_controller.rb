8e6d16133dbaebaf3c14707293b518037838bb51
Tom Kitson <thomaspfkitson@gmail.com>
2022-11-29 18:09:48 +0100

    basic shops controller

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,5 +1,43 @@
 class ShopsController < ApplicationController
+  before_action :set_shop, only: [:show, :edit, :update, :destroy]
+
   def index
     @shops = Shop.all
+  end
+
+  def new
+    @country = request.location.country_code
+    @shop = Shop.new
+  end
+
+  def create
+    @shop = Shop.new(shop_params)
+    @shop.user = current_user
+    @shop.save
+  end
+
+  def show
+    @products = @shop.products
+  end
+
+  def edit
+  end
+
+  def update
+    if @shop.update(shop_params)
+      redirect_to shop_path(@shop)
+    else
+      render "edit", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def shop_params
+    params.require(:shop).permit(:name, :address, :phone_number)
+  end
+
+  def set_shop
+    @shop = Shop.find(params[:id])
   end
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,3 +1,5 @@
 class Shop < ApplicationRecord
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode
 end
