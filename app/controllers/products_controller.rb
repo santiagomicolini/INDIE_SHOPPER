2e5a1d9058f6ee30be85f3b5a2ba96d3dfd2b076
Santiago Micolini <santi.micolini2@gmail.com>
2022-11-29 18:03:44 +0100

    creating product controller, action and views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,24 @@
 class ProductsController < ApplicationController
+  before_action :set_shop, :only[:show, :new, :create]
+  before_action :set_product, :only[:show]
+
+  def new
+    Product.new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  private
+
+  def set_shop
+    @shop = Shop.find(params[:shop_id])
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
 end
