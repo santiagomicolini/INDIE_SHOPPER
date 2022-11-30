772f18917f3d373d4c0fe738957f06e3b0b5dfca
Tom Kitson <thomaspfkitson@gmail.com>
2022-11-30 18:01:13 +0100

    half working shop reviews

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
   private
 
   def skip_pundit?
-    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)|(^reviews$)/
   end
 end
