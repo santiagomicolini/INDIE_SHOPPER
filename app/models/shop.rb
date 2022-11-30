4f3db54fd147d2ba5f912d652b6dcfc00674a261
Santiago Micolini <santi.micolini2@gmail.com>
2022-11-30 15:03:53 +0100

    show page products

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,6 +1,7 @@
 class Shop < ApplicationRecord
   belongs_to :user
-  geocoded_by :address
-  after_validation :geocode
+  belongs_to :shop_category
+  # geocoded_by :address
+  # after_validation :geocode
   has_many_attached :photos
 end
