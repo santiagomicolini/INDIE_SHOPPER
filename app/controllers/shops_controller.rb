70a57c695f539c53ad7acedebedfa45bb358eeaa
Gabriel Martinez <ghmartinez96@hotmail.com>
2022-11-30 16:25:23 +0100

    do not merge

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,4 +1,5 @@
 class ShopsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_shop, only: [:show, :edit, :update, :destroy]
 
   def index
