e49f046af87d653acd57403354a8b1bd1442957b
Gabriel Martinez <ghmartinez96@hotmail.com>
2022-12-01 17:57:40 +0100

    Map inserted. Front end developed.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_30_162919) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_01_113133) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -130,6 +130,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_162919) do
     t.datetime "updated_at", null: false
     t.bigint "shop_category_id", null: false
     t.string "website"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["shop_category_id"], name: "index_shops_on_shop_category_id"
     t.index ["user_id"], name: "index_shops_on_user_id"
   end
