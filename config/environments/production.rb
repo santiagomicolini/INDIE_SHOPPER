25996fe4a0cfa14b9f8e2057b711e6651a62c573
Tom Kitson <thomaspfkitson@gmail.com>
2022-12-08 10:48:11 +0100

    config email for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,20 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
+  config.action_mailer.default_url_options = { host: "http://www.indieshopper.co/" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'indieshoppa@gmail.com',
+    password:             'qqinajcyibnyrnvm',
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5
+  }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
